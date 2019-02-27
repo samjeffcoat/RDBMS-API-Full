09146a7f869d20b09f8c9b0a13f4582c1da2ad74
samjeffcoat <samjeffcoat@gmail.com>
2019-02-27 17:19:07 -0600

    Adds cohorts and students

---
+++ migrations/20190227164827_cohorts.js
@@ -0,0 +1,18 @@
+
+exports.up = function(knex, Promise) {
+  return knex.schema.createTable
+  ('cohorts', function(tbl) {
+      //primary key called id
+
+      tbl.increments();
+      tbl.string('name', 128)
+      .notNullable()
+      .unique()
+      tbl.timestamps(true, true);
+  })
+};
+
+exports.down = function(knex, Promise) {
+  return
+  knex.schema.dropTableIfExists('cohorts');
+};
