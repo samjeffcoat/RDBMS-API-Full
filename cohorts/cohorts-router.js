c0b04a76990399bc0de6b7ec661f9b2fd3f72697
samjeffcoat <samjeffcoat@gmail.com>
2019-02-27 19:13:25 -0600

    Sets up cohorts-router

---
+++ cohorts/cohorts-router.js
@@ -0,0 +1,12 @@
+const router = require('express').Router();
+
+const knex= require('knex')
+
+const knexConfig = require('../knexfile.js');
+
+const db= knex(knexConfig.development)
+
+
+
+
+module.exports= router;
